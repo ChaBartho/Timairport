90f7ddc4c17130ade9290012a6521aa8b113eea6
Charlotte Bartholomé <charlotte.bartholome@gmail.com>
2023-01-26 17:02:15 +0100

    v2

---
+++ src/main/java/be/technifutur/java/timairport/controller/PilotController.java
@@ -0,0 +1,29 @@
+package be.technifutur.java.timairport.controller;
+
+import be.technifutur.java.timairport.model.dto.PilotDTO;
+import be.technifutur.java.timairport.service.PilotService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController //= @Controller + @Responsbody
+@RequestMapping("/pilot") //= pour ne pas écrire "pilot" à chaque mapping
+public class PilotController {
+
+    private final PilotService pilotService;
+
+    public PilotController(PilotService pilotService) {
+        this.pilotService = pilotService;
+    }
+
+    //Recuperer les pilots
+    @GetMapping("/all")
+    public List<PilotDTO> getAll(){
+        return pilotService.getAll();
+    }
+
+
+
+}
